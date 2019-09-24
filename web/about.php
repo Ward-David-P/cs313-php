01f78f680513f5d6a722f422eb7e4845eb719e9f
crazyivan85204 <crazyivan85204@yahoo.com>
2019-09-24 11:51:00 -0700

    added about page and updates to css

---
+++ web/about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>About Me and Stuff</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<header>
+    <?php
+    include_once 'php/nav_bar.php';
+    ?>
+</header>
+<body class="container-fluid">
+    <h1>About Me and Stuff</h1>
+    <div class="info col-sm-4" id="aboutDiv">
+        <p>My name is David Ward. I am from Mesa, Arizona USA. I served in the Ohio Cleveland Mission. I am studying
+            Software Engineering and would like to develop educational software and tools. Currently, I work as a
+            Solutions Engineer for GoDaddy. I love to do things outdoors. Hiking, backpacking, climbing, and camping
+            are just a few of my hobbies. Right now I am really into bouldering. I enjoy eating many things, mostly
+            stuff that isn't good for you. I especially love it when I can do these things with my wife, son (6), and
+            daughter (3). I also enjoy reading comic books/graphic novels (particularly DC), video games, movies/TV,
+            and reading.</p>
+    </div>
+</body>
+<footer>
+    <?php
+    include_once 'php/footer.php';
+    ?>
+</footer>
--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
-    <title>W02 Team Assignment</title>
+    <title>The Me Site</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
@@ -16,8 +16,9 @@
 </header>
 <body class="container-fluid">
     <h1>The Me Site</h1>
-    <div class="info" id="homeDiv">
-
+    <div class="info col-sm-4" id="homeDiv">
+        <p>Hi, my name is David. To learn more about me and some other stuff go to my <a href="about.php">about</a>
+            page. Really, you should go there. There isn't really anything else here.</p>
     </div>
 </body>
 <footer>
