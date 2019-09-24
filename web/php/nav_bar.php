a8d26e93e20866a659276ffcbd521defb850eca7
crazyivan85204 <crazyivan85204@yahoo.com>
2019-09-24 11:34:15 -0700

    updates w02 prove

--- web/php/nav_bar.php
+++ web/php/nav_bar.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
-<div class="topNav">
-    <a class="navLinks" href="../index.php">Home</a>
-    <a class="navLinks" href="../assignments.php">Assignments</a>
-    <a class="navLinks" href="../about.php">About Stuff</a>
-</div>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body class="container-fluid">
+    <div class="topNav">
+        <a class="navLinks" href="../index.php">Home</a>
+        <a class="navLinks" href="../assignments.php">Assignments</a>
+        <a class="navLinks" href="../about.php">About Stuff</a>
+    </div>
+</body>
