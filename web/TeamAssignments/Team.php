0e8e098afb712d3faf1e4dfe96eeef6ecb90fc13
crazyivan85204 <crazyivan85204@yahoo.com>
2019-10-01 07:51:44 -0700

    team assignment 3

--- web/TeamAssignments/Team.php
+++ web/TeamAssignments/Team.php
@@ -13,6 +13,13 @@
             <input type="radio" name="major" value="wdd">Web Design and Dev.
             <input type="radio" name="major" value="cit">Computer Info Tech
             <input type="radio" name="major" value="ce">Computer Engineering
+            <input type="checkbox" name="continents" value="North America">North America
+            <input type="checkbox" name="continents" value="South America">South America
+            <input type="checkbox" name="continents" value="Europe">Europe
+            <input type="checkbox" name="continents" value="Asia">Asia
+            <input type="checkbox" name="continents" value="Australia">Australia
+            <input type="checkbox" name="continents" value="Africa">Africa
+            <input type="checkbox" name="continents" value="Antarctica">Antarctica
             Comment: <textarea name="comment" rows="5" cols="40"></textarea>
             <input type="submit">
         </form>
