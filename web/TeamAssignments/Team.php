190090f1c0038c77f9e23ef7ebd4fe221a89c923
crazyivan85204 <crazyivan85204@yahoo.com>
2019-10-01 08:00:48 -0700

    team assignment 3

--- web/TeamAssignments/Team.php
+++ web/TeamAssignments/Team.php
@@ -9,16 +9,18 @@
         <form action="form.php" method="post">
             Name: <input type="text" name="name"><br>
             E-mail: <input type="text" name="email"><br>
-            <input type="radio" name="major" value="cs">Computer Science
-            <input type="radio" name="major" value="wdd">Web Design and Dev.
-            <input type="radio" name="major" value="cit">Computer Info Tech
-            <input type="radio" name="major" value="ce">Computer Engineering
             <?php
+                $continent = array("North America" => "North America", "South America" => "South America", "Europe" => "Europe", "Asia" => "Asia", "Australia" => "Australia", "Africa" => "Africa", "Antarctica" => "Antarctica");
+
+                foreach($continent as $x => $x_value){
+                    echo '<input type="Checkbox" name="continents" value="' . $x . '"> ' . $x_value . '<br/>';
+                }
+
                 $major = array("cs" => "Computer Science", "wdd" => "Web Design and Development", "cit" => "Computer Information Technology", "ce" => "Computer Engineering");
 
                 foreach($major as $x => $x_value){
                     echo '<input type="radio" name="major" value="' . $x . '"> ' . $x_value . '<br/>';
-                }
+            }
             ?>
             Comment: <textarea name="comment" rows="5" cols="40"></textarea>
             <input type="submit">
