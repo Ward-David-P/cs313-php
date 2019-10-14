bb051c3b79abf28c7d4d723b67c02ba65d561b22
crazyivan85204 <crazyivan85204@yahoo.com>
2019-10-14 14:19:27 -0700

    prove03

--- web/ProveAssign/Prove03/php/functions.php
+++ web/ProveAssign/Prove03/php/functions.php
@@ -3,6 +3,5 @@ session_start();
 
 function addToSession($product)
 {
-    $_SESSION['cartItems'] = array();
-    array_push($_SESSION['cartItems'], $product);
+    $_SESSION[$product] = $product;
 }
