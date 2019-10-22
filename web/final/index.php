5ba9cbb2d70d3a5228c5e3e9c4bf4ccf788553eb
crazyivan85204 <crazyivan85204@yahoo.com>
2019-10-22 10:42:44 -0700

    title updates

--- web/final/index.php
+++ web/final/index.php
@@ -2,7 +2,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
-    <title>Assignments</title>
+    <title>Comicbook Library</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
