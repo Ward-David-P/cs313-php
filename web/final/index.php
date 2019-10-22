2e2e206c2bed49c30d4f95187d20019cdde7a190
crazyivan85204 <crazyivan85204@yahoo.com>
2019-10-22 15:18:50 -0700

    commit

--- web/final/index.php
+++ web/final/index.php
@@ -30,6 +30,7 @@
                     $username = htmlspecialchars($_POST['username']);
                     $password = htmlspecialchars($_POST['password']);
                     $loginQuery = "SELECT user_id FROM cb_users WHERE username = '".$username."' AND password = MD5('".$password."'";
+                    $db = get_db();
                     $row = $db->query($loginQuery);
                 
                     if (pg_num_rows($row) > 0)
